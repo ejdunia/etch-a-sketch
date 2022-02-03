e889038a419e5806e323a35cf0cfac4d6787b11e
ejdunia <ejdunia@gmail.com>
2022-02-03 04:08:07 +0100

    added color buttons and eraser

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,81 @@
-let slider = document.getElementById("myRange");
-let gridNumberOutput = document.getElementById("gridNumberOutput");
 let gridNum = 16;
-const sketchContainer = document.querySelector(".sketchContainer");
-const gridDiv = document.createElement("div");
 
-gridNumberOutput.textContent = `${slider.value} X ${slider.value}`; // Display the default slider value
+const slider = document.getElementById("myRange");
+
+const gridNumberOutput = document.getElementById("gridNumberOutput");
+const pickerContainer = document.querySelector(".pickerContainer");
+const colorPicker = document.querySelector("#colorPicker");
+
+const sketchContainer = document.querySelector(".sketchContainer");
+
+const gridDiv = document.createElement("div");
+const grids = document.querySelectorAll("gridDiv");
+
+const eraser = document.querySelector(".eraser");
+const rainbow = document.querySelector(".rainbow");
+let colorPick = colorPicker.value;
+const clear = document.querySelector(".clear");
+
+const chooseColor = document.querySelector(".chooseColor");
 
 function changeBGColor(e) {
     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     e.target.style.background = `#${randomColor}`;
 }
+function defaultColor(e) {
+    e.target.style.background = "#f0f8ff";
+}
+
+function pickedColor(e) {
+    e.target.style.background = colorPick;
+}
+
 function makeGrid() {
     sketchContainer.setAttribute(
         "style",
         `grid-template-columns: repeat(${gridNum}, 1fr);
-    grid-template-rows: repeat(${gridNum}, 1fr);
-`
+        grid-template-rows: repeat(${gridNum}, 1fr);`
     );
+
     for (let i = 0; i < gridNum ** 2; i++) {
         const gridDiv = document.createElement("div");
         gridDiv.classList.add("gridDiv");
         gridDiv.addEventListener("mouseover", changeBGColor);
         sketchContainer.appendChild(gridDiv);
-        const reset = document.querySelector(".resetGrid");
 
-        reset.addEventListener(
-            "click",
-            () => (gridDiv.style.background = "#f0f8ff")
+        clear.addEventListener("click", () => {
+            gridDiv.style.background = "#f0f8ff";
+        });
+
+        rainbow.addEventListener("click", () =>
+            gridDiv.addEventListener("mouseover", changeBGColor)
         );
+
+        eraser.addEventListener("click", () => {
+            gridDiv.removeEventListener("mouseover", changeBGColor);
+            gridDiv.addEventListener("mouseover", defaultColor);
+        });
+        chooseColor.addEventListener("click", () => {
+            gridDiv.removeEventListener("mouseover", changeBGColor);
+            gridDiv.addEventListener("mouseover", pickedColor);
+        });
+        // clear.addEventListener("click", defaultColor);
     }
 }
 
+gridNumberOutput.textContent = `${slider.value} X ${slider.value}`;
+
+colorPicker.addEventListener("input", () => {
+    pickerContainer.style.background = colorPicker.value;
+    colorPick = colorPicker.value;
+});
+
+pickerContainer.style.background = colorPicker.value;
+
 makeGrid((gridNum = 16));
 
-slider.oninput = function () {
-    // Update the current slider value (each time you drag the slider handle)
-    // let gridNum = slider.value;
-    gridNumberOutput.textContent = this.value;
+slider.addEventListener("input", () => {
+    gridNumberOutput.textContent = `${slider.value} X ${slider.value}`;
     gridNum = slider.value;
-    //     sketchContainer.setAttribute(
-    //         "style",
-    //         `grid-template-columns: repeat(${gridNum}, 1fr);
-    //     grid-template-rows: repeat(${gridNum}, 1fr);
-    // `    );
     makeGrid();
-};
+});
